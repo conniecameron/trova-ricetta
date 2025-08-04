a78ad3c05f146448bbb7844d667a0defb9068209
conniecameron <esguerra.connie@gmail.com>
2025-08-03 22:52:00 -0500

    ran init.sql, 3 tables created

--- db/mysql_repository.py
+++ db/mysql_repository.py
@@ -1,6 +1,8 @@
 import mysql.connector
+from db.interface import *
 
-class MysqlRepository:
+
+class MysqlRepository(Repository):
 
     def __init__(self):
         config = {
@@ -10,11 +12,9 @@ class MysqlRepository:
             #Github Push
             #'host': 'mysql',  # When you run this on your machine change it to 'localhost'
             #'port': '3306',  # When you run this on your machine change it to '32000'
-            
             #Local Machine
             'host': 'localhost',  # When you run this on your machine change it to 'localhost'
             'port': '32000',  # When you run this on your machine change it to '32000'
-
             'database': 'ricetta'
         }
         self.connection = mysql.connector.connect(**config)
