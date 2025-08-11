ecce3ee118d70bd16fa8ee73ab63d32eb8e9525f
conniecameron <esguerra.connie@gmail.com>
2025-08-10 22:25:47 -0500

    updated UML diagram based on grading feedback

--- db/mysql_repository.py
+++ db/mysql_repository.py
@@ -10,11 +10,11 @@ class MysqlRepository(Repository):
             'password': 'root',
 
             #Github Push
-            'host': 'mysql',  # When you run this on your machine change it to 'localhost'
-            'port': '3306',  # When you run this on your machine change it to '32000'
+            #'host': 'mysql',  # When you run this on your machine change it to 'localhost'
+            #'port': '3306',  # When you run this on your machine change it to '32000'
             #Local Machine
-            #'host': 'localhost',  # When you run this on your machine change it to 'localhost'
-            #'port': '32000',  # When you run this on your machine change it to '32000'
+            'host': 'localhost',  # When you run this on your machine change it to 'localhost'
+            'port': '32000',  # When you run this on your machine change it to '32000'
             'database': 'ricetta'
         }
         self.connection = mysql.connector.connect(**config)
